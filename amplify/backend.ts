f452a0db99f3ef16feb8b32fbff49a4c51e47f14
Yifan-rgb <Yifanzhang1219@outlook.com>
2024-06-05 16:20:14 +0100

    change storage  back to default

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,22 +2,22 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
-//import { storage } from './storage/resource';
+import { storage } from './storage/resource';
 
-// defineBackend({
-//   auth,
-//   data,
-//   storage,
-// });
-
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
+  storage,
 });
 
-backend.addOutput({
-  storage: {
-    aws_region: "eu-west-2",
-    bucket_name: "testbucket-bduvi-neoscb"
-  },
-});
+// const backend = defineBackend({
+//   auth,
+//   data,
+// });
+
+// backend.addOutput({
+//   storage: {
+//     aws_region: "eu-west-2",
+//     bucket_name: "testbucket-bduvi-neoscb"
+//   },
+// });
