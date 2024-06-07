9c8ebe1da70dca05cb37e88664d38736417ad1e8
Yifan-rgb <Yifanzhang1219@outlook.com>
2024-06-07 17:39:25 +0100

    autoCreatedBucket

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -4,20 +4,20 @@ import { data } from './data/resource';
 
 import { storage } from './storage/resource';
 
-// defineBackend({
-//   auth,
-//   data,
-//   storage,
-// });
-
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
+  storage,
 });
 
-backend.addOutput({
-  storage: {
-    aws_region: "eu-west-2",
-    bucket_name: "testbucket-bduvi-neoscb"
-  },
-});
+// const backend = defineBackend({
+//   auth,
+//   data,
+// });
+
+// backend.addOutput({
+//   storage: {
+//     aws_region: "eu-west-2",
+//     bucket_name: "testbucket-bduvi-neoscb"
+//   },
+// });
